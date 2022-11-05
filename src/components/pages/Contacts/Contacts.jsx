4446f92ecb8e579e312563adf55c4a91fe56d51d
Protsiuk-Prits Dmytro <78557776+ProtsyukPrits@users.noreply.github.com>
2022-11-05 11:16:56 +0100

    added counter

--- src/components/pages/Contacts/Contacts.jsx
+++ src/components/pages/Contacts/Contacts.jsx
@@ -1,10 +1,15 @@
 import ContactList from "components/ContactList"
 import Filter from "components/Filter"
-import { ContactsContainer } from "./Contacts.styled"
-
+import { ContactsContainer, Counter } from "./Contacts.styled"
+import { getContacts } from "redux/selectors"
+import { useSelector } from "react-redux"
 const Contacts = () => {
+
+  const contacts = useSelector(getContacts);
+
   return (
     <ContactsContainer>
+      <Counter>You have {contacts.length} contacts</Counter>
       <Filter />
       <ContactList/>
     </ContactsContainer>
--- src/components/pages/Contacts/Contacts.styled.jsx
+++ src/components/pages/Contacts/Contacts.styled.jsx
@@ -4,3 +4,10 @@ export const ContactsContainer = styled.div`
   background-color: #e6e6fa;
   padding: 40px;
 `;
+
+export const Counter = styled.div`
+  padding: 10px 0px;
+  text-decoration: underline;
+  font-weight: 600;
+  
+`;
